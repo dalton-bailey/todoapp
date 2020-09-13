9ca65fc9fb2e2598df2adba50529e04b4671d982
Landon Conover <landon.conover@gmail.com>
2020-09-12 22:56:55 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+//inital data array. DO NOT EDIT OR REMOVE - Use these as the inital app state.
+//This is the kind of data you would traditionally get from a data base.
+//For now we are just going to mock it.
+let initalTodos = [
+    {id: 1, todo: "Buy milk.", complete: false, category: "Grocery"},
+    {id: 1, todo: "Clean the cat box.", complete: false, category: "House"},
+    {id: 1, todo: "Chips and salsa.", complete: false, category: "Grocery"},
+    {id: 1, todo: "Finish Homework for DGM 3760", complete: false, category: "School"}
+]
