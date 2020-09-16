af36caa4fa746a898f17c7264324e97e324d77aa
Bailey Dalton <baidalton@gmail.com>
2020-09-15 21:06:53 -0600

    fixed array

--- src/main.js
+++ src/main.js
@@ -2,43 +2,24 @@
 //This is the kind of data you would traditionally get from a data base.
 //For now we are just going to mock it.
 let initalTodos = [
-    {id: 1, todo: "Buy milk.", complete: false, category: "Grocery"},
-    {id: 1, todo: "Clean the cat box.", complete: false, category: "House"},
-    {id: 1, todo: "Chips and salsa.", complete: false, category: "Grocery"},
-    {id: 1, todo: "Finish Homework for DGM 3760", complete: false, category: "School"}
+    {id: 1, todo: "buy milk", complete: false, category: "Grocery"},
+    {id: 2, todo: "laundry", complete: false, category: "House"},
+    {id: 3, todo: "eggs", complete: false, category: "Grocery"},
+    {id: 4, todo: "hw", complete: false, category: "School"}
 ]
 
-//new array of arrays
-let array = initalTodos.map(obj => Object.values(obj))
-
-console.log(array)
 
 //view all todos
-let list = document.createElement("ul")
-list.className ="myUL"
-let first = document.createElement("li")
-first.innerHTML = array[0][1]
-list.appendChild(first)
-
-let sec = document.createElement("li")
-sec.innerHTML = array[1][1]
-list.appendChild(sec)
-
-let third = document.createElement("li")
-third.innerHTML = array[2][1]
-list.appendChild(third)
-
-let fourth = document.createElement("li")
-fourth.innerHTML = array[3][1]
-list.appendChild(fourth)
-
-document.body.appendChild(list)
-
+initalTodos.map(item => {
+  let todo = document.createElement("li")
+  todo.innerHTML = item.todo 
+  document.body.appendChild(todo)
+})
 
 //filter todos by category
-const groceryItems = array.filter(item => item[3] === "Grocery")
-const houseItems = array.filter(item => item[3] === "House")
-const schoolItems = array.filter(item => item[3] === "School")
+const groceryItems = initalTodos.filter(item => item.category === "Grocery")
+const houseItems = initalTodos.filter(item => item.category === "House")
+const schoolItems = initalTodos.filter(item => item.category === "School")
 
 
 console.log(groceryItems)
@@ -56,12 +37,11 @@ groceryTitle.innerHTML = "Grocery Todos"
 groceries.appendChild(groceryTitle)
 
 //grocery list
-let groceryList = document.createElement("ul")
-groceries.appendChild(groceryList)
-let groceryElement = groceryItems.forEach(item => {
-    let groceryItem = document.createElement("li")
-    groceryItem.innerHTML = item[1]
-    groceryList.appendChild(groceryItem)
+let groceriesList = groceryItems.forEach(item => {
+  let todo = document.createElement("li")
+  todo.innerHTML = item.todo
+
+  groceries.appendChild(todo)
 })
 
 //display grocery list
@@ -75,13 +55,12 @@ let houseTitle = document.createElement("h1")
 houseTitle.innerHTML = "House Todos"
 house.appendChild(houseTitle)
 
-//grocery list
-let houseList = document.createElement("ul")
-house.appendChild(houseList)
-let houseElement = houseItems.forEach(item => {
-    let houseItem = document.createElement("li")
-    houseItem.innerHTML = item[1]
-    houseList.appendChild(houseItem)
+//house list
+let houseList = houseItems.forEach(item => {
+  let todo = document.createElement("li")
+  todo.innerHTML = item.todo
+
+  house.appendChild(todo)
 })
 
 //display house list
@@ -96,36 +75,21 @@ schoolTitle.innerHTML = "School Todos"
 school.appendChild(schoolTitle)
 
 //school list
-let schoolList = document.createElement("ul")
-school.appendChild(schoolList)
-let schoolElement = schoolItems.forEach(item => {
-    let schoolItem = document.createElement("li")
-    schoolItem.innerHTML = item[1]
-    schoolList.appendChild(schoolItem)
+let schoolList = schoolItems.forEach(item => {
+  let todo = document.createElement("li")
+  todo.innerHTML = item.todo
+
+  school.appendChild(todo)
 })
 
-//display grocery list
+//display school list
 document.body.appendChild(school)
 
 //create div for add input and button
 let add = document.createElement("div")
 document.body.appendChild(add)
 
-//get input to create new todo
-let input = document.createElement("input")
-input.type = "text"
-input.placeholder = "Laundry"
-input.id = "newTodo"
-input.className = "userInput"
-add.appendChild(input);
-
-//add button
-let addButton = document.createElement("button")
-addButton.className ="add-button"
-addButton.innerHTML = "Add"
-add.appendChild(addButton)
-
-
+//delete list items
 var myList = document.getElementsByTagName('li')
 var i
 for (i = 0; i < myList.length; i++) {
@@ -145,36 +109,24 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var newList = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+//add button
+let addButton = document.createElement("button")
+addButton.className = "add-button"
+addButton.innerHTML = "Add"
+add.appendChild(addButton)
 
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = input.value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    input.value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  }
+addButton.addEventListener('click', () => {
+  initalTodos.push(Object.values(input.value))
+})
+
+
+//get input to create new todo
+let input = document.createElement("input")
+input.type = "text"
+input.placeholder = "Laundry"
+input.className = "userInput"
+add.appendChild(input);
+
+
+
+
